cf3488fbdce92846a1047096dd1cbd5756929bfe
kimD0ngjun <chickentasty0112@gmail.com>
2024-09-17 21:44:25 +0900

    initial setting

---
+++ src/main/java/com/example/chatservermessage/ChatServerMessageApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatservermessage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatServerMessageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatServerMessageApplication.class, args);
+	}
+
+}
