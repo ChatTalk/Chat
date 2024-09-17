ad9354846a10bfe2ff9bdfcc774ee46098546618
kimD0ngjun <chickentasty0112@gmail.com>
2024-09-18 00:07:23 +0900

    feat: WebFlux 기반 리액티브 방식 메세지매핑 처리

--- src/main/java/com/example/chatservermessage/domain/controller/WebSocketMessageController.java
+++ src/main/java/com/example/chatservermessage/domain/controller/WebSocketMessageController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import reactor.core.publisher.Mono;
 
 import java.security.Principal;
 
@@ -19,41 +20,43 @@ public class WebSocketMessageController {
 
     // 사용자의 채팅방 입장
     @MessageMapping(value = "/chat/enter")
-    public void enter(ChatMessageDTO.Enter enter, Principal principal) {
-        log.info("{}번 채팅방에서 클라이언트로부터 {} 회원이 입장 요청",
-                enter.getChatId(), principal.getName());
+    public Mono<Void> enter(ChatMessageDTO.Enter enter, Principal principal) {
+        return Mono.fromRunnable(() -> {
+            log.info("{}번 채팅방에서 클라이언트로부터 {} 회원이 입장 요청",
+                    enter.getChatId(), principal.getName());
 
-        ChatMessage message = new ChatMessage(enter, principal.getName());
-        ChatMessageDTO dto = new ChatMessageDTO(message);
+            ChatMessage message = new ChatMessage(enter, principal.getName());
+            ChatMessageDTO dto = new ChatMessageDTO(message);
 
-        messagingTemplate.convertAndSend(
-                CHAT_DESTINATION + dto.getChatId(), dto);
+            messagingTemplate.convertAndSend(CHAT_DESTINATION + dto.getChatId(), dto);
+        });
     }
 
     // 사용자의 메세지 입력 송수신
     @MessageMapping(value = "/chat/message")
-    public void message(ChatMessageDTO.Send send, Principal principal) {
-        log.info("{}반 채팅방에서 클라이언트로부터 {} 회원이 메세지 전송 요청: {}",
-                send.getChatId(), principal.getName(), send.getMessage());
+    public Mono<Void> message(ChatMessageDTO.Send send, Principal principal) {
+        return Mono.fromRunnable(() -> {
+            log.info("{}번 채팅방에서 클라이언트로부터 {} 회원이 메세지 전송 요청: {}",
+                    send.getChatId(), principal.getName(), send.getMessage());
 
-        ChatMessage message = new ChatMessage(send, principal.getName());
-        ChatMessageDTO dto = new ChatMessageDTO(message);
+            ChatMessage message = new ChatMessage(send, principal.getName());
+            ChatMessageDTO dto = new ChatMessageDTO(message);
 
-        messagingTemplate.convertAndSend(
-                CHAT_DESTINATION + dto.getChatId(), dto);
+            messagingTemplate.convertAndSend(CHAT_DESTINATION + dto.getChatId(), dto);
+        });
     }
 
     // 사용자의 채팅방 퇴장
     @MessageMapping(value = "/chat/leave")
-    public void leave(ChatMessageDTO.Leave leave, Principal principal) {
-        log.info("{}반 채팅방에서 클라이언트로부터 {} 회원이 퇴장 요청",
-                leave.getChatId(), principal.getName());
+    public Mono<Void> leave(ChatMessageDTO.Leave leave, Principal principal) {
+        return Mono.fromRunnable(() -> {
+            log.info("{}번 채팅방에서 클라이언트로부터 {} 회원이 퇴장 요청",
+                    leave.getChatId(), principal.getName());
 
-        // 채팅 메세지 엔티티 관련 서비스 로직(mongoDB) 수행
-        ChatMessage message = new ChatMessage(leave, principal.getName());
-        ChatMessageDTO dto = new ChatMessageDTO(message);
+            ChatMessage message = new ChatMessage(leave, principal.getName());
+            ChatMessageDTO dto = new ChatMessageDTO(message);
 
-        messagingTemplate.convertAndSend(
-                CHAT_DESTINATION + dto.getChatId(), dto);
+            messagingTemplate.convertAndSend(CHAT_DESTINATION + dto.getChatId(), dto);
+        });
     }
 }
