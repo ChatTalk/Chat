9c61f331d6faa00e64bfb0710ce3f82c04097646
kimD0ngjun <chickentasty0112@gmail.com>
2024-09-20 11:16:35 +0900

    feat: 웹소켓 인터셉터 인증 redis 캐시 데이터 기반으로 수정

--- src/main/java/com/example/chatservermessage/global/message/WebSocketInterceptor.java
+++ src/main/java/com/example/chatservermessage/global/message/WebSocketInterceptor.java
@@ -1,35 +1,28 @@
 package com.example.chatservermessage.global.message;
 
-import com.example.chatservermessage.domain.dto.TokenDTO;
 import com.example.chatservermessage.domain.dto.UserInfoDTO;
 import com.example.chatservermessage.global.user.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.HttpStatus;
-import org.springframework.kafka.core.reactive.ReactiveKafkaProducerTemplate;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
-import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ResponseStatusException;
-import reactor.core.publisher.Mono;
-import reactor.kafka.receiver.KafkaReceiver;
 
-import java.time.Duration;
-import java.util.UUID;
+import static com.example.chatservermessage.global.constant.Constants.COOKIE_AUTH_HEADER;
+import static com.example.chatservermessage.global.constant.Constants.REDIS_ACCESS_KEY;
 
 @Slf4j(topic = "WebSocketInterceptor")
 @Component
@@ -37,70 +30,34 @@ import java.util.UUID;
 @Order(Ordered.HIGHEST_PRECEDENCE + 99)
 public class WebSocketInterceptor implements ChannelInterceptor {
 
-    @Value("${kafka.topic}")
-    private String topic;
-
-    private final ReactiveKafkaProducerTemplate<String, TokenDTO> kafkaProducerTemplate;
-    private final KafkaReceiver<String, UserInfoDTO> kafkaReceiver;
+    private RedisTemplate<String, UserInfoDTO> userInfoTemplate;
 
     @Override
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
-//        log.info("일단 여긴 도착했는지?");
-
         StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
 
-        /**
-         * 일단 여긴 레디스 캐시 조회로 고쳐야겠다. 카프카 처리 넘 빡빡할듯
-         */
-
         if (accessor != null && StompCommand.CONNECT.equals(accessor.getCommand())) {
-            return setAuthenticate(accessor)
-                    .doOnNext(userInfoDTO -> {
-                        if (userInfoDTO != null) {
-                            // 인증이 성공한 경우
-                            String email = userInfoDTO.getEmail();
-                            String role = userInfoDTO.getRole();
-
-                            log.info("소켓 CONNECT 시도, 유저 이메일 : {} // 권한 : {}", email, role);
-
-                            Authentication authentication = this.createAuthentication(userInfoDTO);
-                            SecurityContextHolder.getContext().setAuthentication(authentication);
-                            accessor.setUser(authentication);
-                        } else {
-                            // 인증 실패 시 처리
-                            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Authentication failed");
-                        }
-                    })
-                    .map(userInfoDTO -> MessageBuilder.createMessage(message.getPayload(), accessor.getMessageHeaders()))
-                    .doOnError(ex -> log.error("Error during WebSocket authentication", ex))
-                    .block(); // 동기화된 블로킹 작업
+            this.setAuthenticate(accessor);
         }
-
-//        log.info("message 내용 확인: {}", message.getPayload());
         return message;
     }
 
-    private Mono<UserInfoDTO> setAuthenticate(final StompHeaderAccessor accessor) {
-        String token = accessor.getFirstNativeHeader("Authorization");
-        UUID id = UUID.randomUUID();
-        log.info("추출된 토큰: {} // 아이디: {}", token, id);
+    private void setAuthenticate(final StompHeaderAccessor accessor) {
+        String accessTokenValue = accessor.getFirstNativeHeader(COOKIE_AUTH_HEADER);
 
-        TokenDTO tokenDTO = new TokenDTO(id, token);
+        UserInfoDTO dto = userInfoTemplate.opsForValue().get(REDIS_ACCESS_KEY + accessTokenValue);
 
-        // 비동기적으로 Kafka에 인증 요청 전송
-        return kafkaProducerTemplate.send(topic, id.toString(), tokenDTO)
-                .then(Mono.defer(() -> kafkaReceiver
-                        .receive()
-                        .filter(record -> record.key().equals(id.toString()))
-                        .next()
-                        .map(ConsumerRecord::value)
-                        .timeout(Duration.ofSeconds(10))
-                        .onErrorResume(e -> {
-                            // 인증 실패 시 처리
-                            log.error("Error during authentication", e);
-                            return Mono.empty(); // 인증 실패 시 빈 Mono를 반환
-                        })
-                ));
+        if (dto == null) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Non cache access token");
+        }
+
+        // 좀 더 실용적인 인증 수단 마련 필요
+        String email = dto.getEmail();
+        log.info("소켓 CONNECT 시도, 유저 이메일 : {}", email);
+
+        Authentication authentication = this.createAuthentication(dto);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        accessor.setUser(authentication);
     }
 
     private Authentication createAuthentication(final UserInfoDTO userInfoDTO) {
