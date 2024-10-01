6a7bc3cc7fabc61c80b78b816ba072b56818062a
kimD0ngjun <chickentasty0112@gmail.com>
2024-10-01 22:38:31 +0900

    fix: 컨트롤러와 서비스 분리

--- src/main/java/com/example/chatservermessage/domain/controller/ChatMessageController.java
+++ src/main/java/com/example/chatservermessage/domain/controller/ChatMessageController.java
@@ -2,15 +2,14 @@ package com.example.chatservermessage.domain.controller;
 
 import com.example.chatservermessage.domain.dto.ChatMessageDTO;
 import com.example.chatservermessage.domain.entity.ChatMessage;
+import com.example.chatservermessage.domain.service.ChatMessageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
 
 import java.security.Principal;
-import java.util.Objects;
 
 import static com.example.chatservermessage.global.constant.Constants.*;
 
@@ -20,9 +19,7 @@ import static com.example.chatservermessage.global.constant.Constants.*;
 public class ChatMessageController {
 
     private final SimpMessageSendingOperations messagingTemplate;
-    private final RedisTemplate<String, Integer> maxPersonnelTemplate;
-    private final RedisTemplate<String, String> subscribeTemplate;
-    private final RedisTemplate<String, String> participatedTemplate;
+    private final ChatMessageService chatMessageService;
 
     // 사용자의 채팅방 입장
     @MessageMapping(value = "/chat/enter")
@@ -30,28 +27,7 @@ public class ChatMessageController {
         log.info("{}번 채팅방에서 클라이언트로부터 {} 회원이 입장 요청",
                 enter.getChatId(), principal.getName());
 
-        // 이미 구독 중인 유저의 재접속
-        if (Boolean.TRUE.equals(subscribeTemplate.opsForSet().isMember(REDIS_SUBSCRIBE_KEY + principal.getName(), enter.getChatId()))) {
-            log.info("이미 해당 {}번 채팅방 구독 중인 유저 {}:", enter.getChatId(), principal.getName());
-            return;
-        }
-
-        Integer maxPersonnel = maxPersonnelTemplate.opsForValue().get(REDIS_MAX_PERSONNEL_KEY + enter.getChatId());
-        Long participatedPersonnel = participatedTemplate.opsForList().size(REDIS_PARTICIPATED_KEY + enter.getChatId());
-
-        // 인원이 초과된 채팅창 접속(예외 처리)
-        if (Objects.isNull(participatedPersonnel) || Objects.isNull(maxPersonnel)) {
-            throw new IllegalArgumentException("participatedPersonnel 혹은 maxPersonnel 에 이상 있다!");
-        }
-
-        if (maxPersonnel.longValue() == participatedPersonnel) {
-            throw new IllegalArgumentException("이미 최대인원 가득참");
-        }
-
-        subscribeTemplate.opsForSet()
-                .add(REDIS_SUBSCRIBE_KEY + principal.getName(), enter.getChatId());
-        participatedTemplate.opsForList()
-                .rightPush(REDIS_PARTICIPATED_KEY + enter.getChatId(), principal.getName());
+        chatMessageService.enter(enter, principal);
 
         ChatMessage message = new ChatMessage(enter, principal.getName());
         ChatMessageDTO dto = new ChatMessageDTO(message);
@@ -77,10 +53,7 @@ public class ChatMessageController {
         log.info("{}번 채팅방에서 클라이언트로부터 {} 회원이 퇴장 요청",
                 leave.getChatId(), principal.getName());
 
-        subscribeTemplate.opsForSet()
-                .remove(REDIS_SUBSCRIBE_KEY + principal.getName(), leave.getChatId());
-        participatedTemplate.opsForList()
-                .remove(REDIS_PARTICIPATED_KEY + leave.getChatId(), 0, principal.getName());
+        chatMessageService.leave(leave, principal);
 
         ChatMessage message = new ChatMessage(leave, principal.getName());
         ChatMessageDTO dto = new ChatMessageDTO(message);
---
+++ src/main/java/com/example/chatservermessage/domain/service/ChatMessageService.java
@@ -0,0 +1,58 @@
+package com.example.chatservermessage.domain.service;
+
+import com.example.chatservermessage.domain.dto.ChatMessageDTO;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.security.Principal;
+import java.util.Objects;
+
+import static com.example.chatservermessage.global.constant.Constants.*;
+
+@Slf4j(topic = "ChatMessageService")
+@Service
+@RequiredArgsConstructor
+public class ChatMessageService {
+
+    private final RedisTemplate<String, Integer> maxPersonnelTemplate;
+    private final RedisTemplate<String, String> subscribeTemplate;
+    private final RedisTemplate<String, String> participatedTemplate;
+
+    public void enter(ChatMessageDTO.Enter enter, Principal principal) {
+        // 이미 구독 중인 유저의 재접속
+        if (Boolean.TRUE.equals(subscribeTemplate.opsForSet().isMember(REDIS_SUBSCRIBE_KEY + principal.getName(), enter.getChatId()))) {
+            log.info("이미 해당 {}번 채팅방 구독 중인 유저 {}:", enter.getChatId(), principal.getName());
+            return;
+        }
+
+        Integer maxPersonnel = maxPersonnelTemplate.opsForValue().get(REDIS_MAX_PERSONNEL_KEY + enter.getChatId());
+        Long participatedPersonnel = participatedTemplate.opsForList().size(REDIS_PARTICIPATED_KEY + enter.getChatId());
+
+        // 인원이 초과된 채팅창 접속(예외 처리)
+        if (Objects.isNull(participatedPersonnel) || Objects.isNull(maxPersonnel)) {
+            throw new IllegalArgumentException("participatedPersonnel 혹은 maxPersonnel 에 이상 있다!");
+        }
+
+        if (maxPersonnel.longValue() == participatedPersonnel) {
+            throw new IllegalArgumentException("이미 최대인원 가득참");
+        }
+
+        subscribeTemplate.opsForSet()
+                .add(REDIS_SUBSCRIBE_KEY + principal.getName(), enter.getChatId());
+        participatedTemplate.opsForList()
+                .rightPush(REDIS_PARTICIPATED_KEY + enter.getChatId(), principal.getName());
+    }
+
+    public void message(ChatMessageDTO.Send send, Principal principal) {
+
+    }
+
+    public void leave(ChatMessageDTO.Leave leave, Principal principal) {
+        subscribeTemplate.opsForSet()
+                .remove(REDIS_SUBSCRIBE_KEY + principal.getName(), leave.getChatId());
+        participatedTemplate.opsForList()
+                .remove(REDIS_PARTICIPATED_KEY + leave.getChatId(), 0, principal.getName());
+    }
+}
