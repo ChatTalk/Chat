b21bcd5369f60a86a95cebbea9d0b8e3da08e43f
kimD0ngjun <chickentasty0112@gmail.com>
2024-10-02 11:50:06 +0900

    feat: 미접속 채팅창 메세지 보존 기능 구현 준비

--- src/main/java/com/example/chatservermessage/global/kafka/KafkaMessageService.java
+++ src/main/java/com/example/chatservermessage/global/kafka/KafkaMessageService.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -32,9 +33,17 @@ public class KafkaMessageService {
         }
     }
 
+    /**
+     * 카프카 수동 커밋 관련 내용
+     * -> 이걸 활용해서 접속하지 않았을 때는 메세지 안 읽게 하고 접속해서 채팅창에 접속했을 떄가 되어야 메세지 소비를 가능케 하는..? 가능하려나?
+     * https://dkswnkk.tistory.com/744
+     *
+     수동 커밋은 메시지를 커밋할 시점을 제어할 수 있지만, 나중에 메시지를 읽는 기능은 별도의 저장 및 관리 로직이 필요합니다.
+     사용자가 메시지를 나중에 읽을 수 있도록 하려면, 수신한 메시지를 Redis나 데이터베이스에 저장하고, 사용자가 접속했을 때 이 메시지를 불러오는 로직을 구현해야 합니다.
+     */
     // consumer
     @KafkaListener(topics = KAFKA_CHAT_TOPIC)
-    public void listen(ChatMessageDTO dto) {
+    public void listen(ChatMessageDTO dto, Acknowledgment acknowledgment) {
         log.info("채팅 메세지 수신: {}번 // {}", dto.getChatId(), dto.getMessage());
         messagingTemplate.convertAndSend(CHAT_DESTINATION + dto.getChatId(), dto);
     }
