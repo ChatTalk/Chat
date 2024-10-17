2987de88395c4b92afa2f490abe996e61ba24b9a
kimD0ngjun <chickentasty0112@gmail.com>
2024-10-17 16:48:32 +0900

    feat: 메세지 레포지토리 네이티브 쿼리 적용

--- src/main/java/com/example/chatservermessage/domain/repository/ChatMessageRepository.java
+++ src/main/java/com/example/chatservermessage/domain/repository/ChatMessageRepository.java
@@ -2,8 +2,17 @@ package com.example.chatservermessage.domain.repository;
 
 import com.example.chatservermessage.domain.entity.ChatMessage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
+
+    // 네이티브 쿼리 적용
+    @Query(value = "SELECT * FROM chat_message WHERE chat_id = :chatId AND created >= :startTime AND created <= :endTime", nativeQuery = true)
+    List<ChatMessage> findMessagesByChatIdAndTimeRange(String chatId, LocalDateTime startTime, LocalDateTime endTime);
+
 }
